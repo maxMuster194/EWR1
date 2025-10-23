ad838828491e765a44f659c654529889fd76279b
WiedeJ <127942903+WiedeJ@users.noreply.github.com>
2025-10-23 07:51:00 +0200

    AMberg2 cmt und faktor raus

--- pages/MASTER/Frame/Profil9.js
+++ pages/MASTER/Frame/Profil9.js
@@ -728,7 +728,7 @@ function StrompreisChart({ data, uniqueDates, todayBerlin, error: propsError }) 
           label: function (context) {
             const label = context.dataset.label || '';
             const value = context.raw != null ? context.raw.toFixed(3) : 'N/A';
-            return `${label}: ${value} mCt`; // Einheit geändert zu mCt
+            return `${label}: ${value} Ct`; // Einheit geändert zu Ct
           },
         },
       },
@@ -736,7 +736,7 @@ function StrompreisChart({ data, uniqueDates, todayBerlin, error: propsError }) 
     scales: {
       y: {
         beginAtZero: false,
-        title: { display: true, text: 'Stromkosten in mCt', font: { size: 14, family: "'Inter', sans-serif" }, color: '#FFFFFF' }, // Einheit geändert zu mCt
+        title: { display: true, text: 'Stromkosten in Ct', font: { size: 14, family: "'Inter', sans-serif" }, color: '#FFFFFF' }, // Einheit geändert zu Ct
         ticks: { 
           callback: (value) => `${value.toFixed(2)}`,
           color: '#FFFFFF'
